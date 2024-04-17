6e37fba74edf7bf0a2df5e216a4c5213884d0b77
Phinehas Beresford <78656905+Cracko298@users.noreply.github.com>
2024-04-16 20:50:18 -0400

    Create helpers.asm

---
+++ helpers.asm
@@ -0,0 +1,17 @@
+LAB_0067c46c                                    XREF[1]:     0067c128(j)  
+0067c484 47 2f 8d e2     add        param_3,sp,#0x11c
+0067c48c 63 0b ee fa     blx        FUN_001ff220
+0067c4a0 12 2e 8d e2     add        param_3,sp,#0x120
+0067c4a8 5c 0b ee fa     blx        FUN_001ff220
+0067c4bc 49 2f 8d e2     add        param_3,sp,#0x124
+0067c4c4 55 0b ee fa     blx        FUN_001ff220
+0067c4d8 4a 2f 8d e2     add        param_3,sp,#0x128
+0067c4e0 4e 0b ee fa     blx        FUN_001ff220
+0067c4f4 4b 2f 8d e2     add        param_3,sp,#0x12c
+0067c4fc 47 0b ee fa     blx        FUN_001ff220
+0067c510 13 2e 8d e2     add        param_3,sp,#0x130
+0067c518 40 0b ee fa     blx        FUN_001ff220
+0067c52c 4d 2f 8d e2     add        param_3,sp,#0x134
+0067c534 39 0b ee fa     blx        FUN_001ff220
+0067c548 4e 2f 8d e2     add        param_3,sp,#0x138
+0067c550 32 0b ee fa     blx        FUN_001ff220
