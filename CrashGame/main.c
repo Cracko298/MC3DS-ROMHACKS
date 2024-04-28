ca896e4b4a8f7d09443bbf48f1597d5ba862916f
Phinehas Beresford <78656905+Cracko298@users.noreply.github.com>
2024-04-28 17:36:10 -0400

    Create main.c

---
+++ CrashGame/main.c
@@ -0,0 +1,20 @@
+int main () {
+  int auStack_d8 = 33;
+  int auStack_f4 = 0x0F;
+  int auStack_f0 = 6;
+  int auStack_ec = 33;
+  int auStack_e8 = 35;
+  int auStack_e4 = 0x8E;
+  int auStack_e0 = 0x63;
+  int auStack_dc = 0x2F;
+  const char* bVar2 = 's_ctrl_autojump_0067c860';
+
+  FUN_001ff220(DAT_0067c870,(uint *)s_ctrl_autojump_0067c860,auStack_f4,bVar2);
+  FUN_001ff220(DAT_0067c888,(uint *)s_ctrl_togglecrouch_0067c874,auStack_f0,bVar2);
+  FUN_001ff220(DAT_0067c898,(uint *)s_ctrl_type_0067c88c,auStack_ec,bVar2);
+  FUN_001ff220(DAT_0067c8a4,(uint *)&DAT_0067c89c,auStack_e8,bVar2);
+  FUN_001ff220(DAT_0067c8b8,(uint *)s_game_language_0067c8a8,auStack_e4,bVar2);
+  FUN_001ff220(DAT_0067c8cc,(uint *)s_game_difficulty_0067c8bc,auStack_e0,bVar2);
+  FUN_001ff220(DAT_0067c8e4,(uint *)s_ctrl_autojump_0067c860,auStack_dc,bVar2);
+  FUN_001ff220(DAT_0067c8fc,(uint *)s_game_difficulty_new_0067c8d0,auStack_d8,bVar2);
+}
